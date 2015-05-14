816e84f2cb7e6ee2acac184ee7d559df48f446ee
Ravi Sharma <ravis4376@gmail.com>
2015-05-14 12:16:11 +0530

    third Assignment

--- app/src/main/java/android/bootcamp/projectplanner/NumberOfIterationActivity.java
+++ app/src/main/java/android/bootcamp/projectplanner/NumberOfIterationActivity.java
@@ -3,18 +3,73 @@ package android.bootcamp.projectplanner;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class NumberOfIterationActivity extends Activity {
 
+    private Float total_number_of_iterations;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e("NumberOfIteratActivity", "onCreate");    
         super.onCreate(savedInstanceState);
         setContentView(R.layout.number_of_iterations);
 
         TextView numbersOfIterations = (TextView) findViewById(R.id.number_of_iterations);
 
         Intent intent = getIntent();
-        numbersOfIterations.setText(getString(R.string.iteration_needed)+intent.getFloatExtra("total_number_of_iterations",0.0f));
+        total_number_of_iterations = intent.getFloatExtra("total_number_of_iterations", 0.0f);
+        numbersOfIterations.setText(getString(R.string.iteration_needed)+ total_number_of_iterations);
     }
+
+    public void calculateWithBuffer(View view) {
+        EditText iteration_buffer = (EditText) findViewById(R.id.buffer);
+
+        Float buffer = Float.parseFloat(iteration_buffer.getText().toString());
+
+        Intent intent = new Intent();
+        intent.putExtra("iterations_with_buffer", buffer+total_number_of_iterations);
+        setResult(RESULT_OK, intent);
+        finish(); // destroys the activity
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("NumberOfIteratActivity", "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("NumberOfIteratActivity","onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("NumberOfIteratActivity","onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("NumberOfIteratActivity","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("NumberOfIteratActivity", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("NumberOfIteratActivity", "onPause");
+    }
+
 }
--- app/src/main/java/android/bootcamp/projectplanner/ProjectPlannerActivity.java
+++ app/src/main/java/android/bootcamp/projectplanner/ProjectPlannerActivity.java
@@ -3,6 +3,7 @@ package android.bootcamp.projectplanner;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,8 +27,53 @@ public class ProjectPlannerActivity extends Activity {
     Float noOfIteration = points/velocity;
 
     Intent intent = new Intent(this, NumberOfIterationActivity.class);
-    intent.putExtra("total_number_of_iterations",noOfIteration);
-    startActivity(intent);
+    intent.putExtra("total_number_of_iterations", noOfIteration);
+    startActivityForResult(intent, 1);
     //Project Planner Activity will go in stop state
+  }
+
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode==1){
+      if(resultCode==RESULT_OK){
+        TextView buffer_view = (TextView) findViewById(R.id.iterations_with_buffer);
+        buffer_view.setText(getString(R.string.calculate_with_buffer) + data.getFloatExtra("iterations_with_buffer",0.0f));
+      }
+    }
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.e("ProjectPlannerActivity","onStart");
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    Log.e("ProjectPlannerActivity","onRestart");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.e("ProjectPlannerActivity","onResume");
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.e("ProjectPlannerActivity","onStop");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.e("ProjectPlannerActivity","onDestroy");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.e("ProjectPlannerActivity","onPause");
   }
 }
