21ebdc657fc68a5ecc83fdbe4d0f5bf9f3840b37
Ravi Sharma <ravis4376@gmail.com>
2015-05-14 11:03:06 +0530

    First Assignment

--- app/src/main/java/android/bootcamp/projectplanner/ProjectPlannerActivity.java
+++ app/src/main/java/android/bootcamp/projectplanner/ProjectPlannerActivity.java
@@ -3,6 +3,7 @@ package android.bootcamp.projectplanner;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -11,11 +12,20 @@ public class ProjectPlannerActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_project_planner);
+    setContentView(R.layout.activity_project_planner); //looks in layout folder for given xml
+//    parses Xml and creates View Heirarchy and creates View Object ( Button,Text )
   }
 
   public void calculate(View view) {
-    TextView result = (TextView) findViewById(R.id.number_of_iterations);
-    result.setText("25");
+    TextView number_of_iterations = (TextView) findViewById(R.id.number_of_iterations);
+    EditText iterationPoints = (EditText) findViewById(R.id.number_of_points);
+    EditText iterationVelocity = (EditText) findViewById(R.id.velocity);
+
+    float points = Float.parseFloat(iterationPoints.getText().toString());
+    float velocity = Float.parseFloat(iterationVelocity.getText().toString());
+
+    Float noOfIteration = points/velocity;
+    number_of_iterations.setText(noOfIteration.toString());
+//    number_of_iterations.setText(R.string.app_name);  to access string const
   }
 }
